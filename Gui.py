ddf06a00df9d3145f48d5c51a7f46759e6885e26
Влада Карнаух <vladakarnauh@Vladas-MacBook-Air.local>
2023-12-06 16:06:46 +0000

    trying new AND stuff

--- Gui.py
+++ Gui.py
@@ -29,77 +29,53 @@ with open('MANUFACTURER.csv') as csv_file:
 
 
 # Filter functions
-def filter_property(prop_str, property_var):
-    print(property_var.get())
-    if property_var.get() == 1:
+def filter_via_properties():
+    if fw_var or heli_var or gyro_var or light_var or medium_var or heavy_var:
         filtered_plane_list.clear()
 
-        listbox_content = list_box.get(0, tk.END)
-
         for plane_row in plane_row_list:
-            if plane_row not in listbox_content:
-                if prop_str in plane_row or prop_str + ' ' in plane_row:
+            if fw_var.get():
+                if "Fixed-wing" in plane_row:
                     filtered_plane_list.append(plane_row)
+            if heli_var.get():
+                if "Helicopter" in plane_row:
+                    filtered_plane_list.append(plane_row)
+            if gyro_var.get():
+                if "Gyrocopter" in plane_row:
+                    filtered_plane_list.append(plane_row)
+            if light_var.get():
+                if "Light" or "Light " in plane_row:
+                    filtered_plane_list.append(plane_row)
+            if medium_var.get():
+                if "Medium" or "Medium " in plane_row:
+                    filtered_plane_list.append(plane_row)
+            if heavy_var.get():
+                if "Heavy" or "Heavy" in plane_row:
+                    filtered_plane_list.append(plane_row)
+
         for index, plane_row in enumerate(filtered_plane_list):
             list_box.insert(index, plane_row)
         print("added " + str(filtered_plane_list))
-    else:
-        # create a copy of filtered_plane_list before iterating and modifying it
-        # TODO: Get vehicle class items from plane row list
-        copy_plane_row_list = list(plane_row_list)
-        for plane_row in copy_plane_row_list:
-            if prop_str in plane_row or prop_str + ' ' in plane_row:
-                # filtered_plane_list.remove(plane_row)
-                # convert plane_row to a tuple before finding the index
-                plane_row_tuple = tuple(plane_row)
-                idx = list_box.get(0, tk.END).index(plane_row_tuple)
-                # delete the item at the found index
-                list_box.delete(idx)
-        print("removed " + prop_str)
-
-def filter_fw():
-    filter_property("Fixed-wing", fw_var)
-
-def filter_heli():
-    filter_property("Helicopter", heli_var)
-
-
-def filter_gyro():
-    filter_property("Gyrocopter", gyro_var)
-
-
-def filter_light():
-    filter_property("Light", light_var)
-
-
-def filter_medium():
-    filter_property("Medium", medium_var)
-
-
-def filter_heavy():
-    filter_property("Heavy", heavy_var)
-
-
-def filter_(event):
-    pass
-
-
-def filter_(event):
-    pass
-
-
-def filter_(event):
-    pass
-
-
-
+    # else:
+    #     # create a copy of filtered_plane_list before iterating and modifying it
+    #     # TODO: Get vehicle class items from plane row list
+    #     copy_plane_row_list = list(plane_row_list)
+    #     for plane_row in copy_plane_row_list:
+    #         if prop_str in plane_row or prop_str + ' ' in plane_row:
+    #             # filtered_plane_list.remove(plane_row)
+    #             # convert plane_row to a tuple before finding the index
+    #             plane_row_tuple = tuple(plane_row)
+    #             idx = list_box.get(0, tk.END).index(plane_row_tuple)
+    #             # delete the item at the found index
+    #             list_box.delete(idx)
+    #     print("removed " + prop_str)
 
 
 tk.Label(frm, text="Class").grid(column=0, row=0)
 
-fw_check = tk.Checkbutton(frm, text="Fixed-wing", variable=fw_var, command=filter_fw)
-heli_check = tk.Checkbutton(frm, text="Helicopter", variable=heli_var, command=filter_heli)
-gc_check = tk.Checkbutton(frm, text="Gyrocopter", variable=gyro_var, command=filter_gyro)
+fw_check = tk.Checkbutton(frm, text="Fixed-wing", variable=fw_var, command=filter_via_properties)
+heli_check = tk.Checkbutton(frm, text="Helicopter", variable=heli_var, command=filter_via_properties)
+gc_check = tk.Checkbutton(frm, text="Gyrocopter", variable=gyro_var, command=filter_via_properties)
 
 fw_check.grid(column=0, row=1)
 heli_check.grid(column=0, row=2)
@@ -107,9 +83,9 @@ gc_check.grid(column=0, row=3)
 
 tk.Label(frm, text="ICAO WTC").grid(column=0, row=5)
 
-light_check = tk.Checkbutton(frm, text="Light", variable=light_var, command=filter_light)
-med_check = tk.Checkbutton(frm, text="Medium", variable=medium_var, command=filter_medium)
-heavy_check = tk.Checkbutton(frm, text="Heavy", variable=heavy_var, command=filter_heavy)
+light_check = tk.Checkbutton(frm, text="Light", variable=light_var, command=filter_via_properties)
+med_check = tk.Checkbutton(frm, text="Medium", variable=medium_var, command=filter_via_properties)
+heavy_check = tk.Checkbutton(frm, text="Heavy", variable=heavy_var, command=filter_via_properties)
 
 light_check.grid(column=0, row=6)
 med_check.grid(column=0, row=7)
